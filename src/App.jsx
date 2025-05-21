1473e26968525764650362013137e1b4d6ed04a9
Rudvan <rudvancastro@gmail.com>
2025-05-21 11:18:30 +0200

    Replace NavbarGuest with NavbarAuth in App component; wrap FilmInfo content in a fragment for improved structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Home from "@pages/auth/Home";
 import FilmInfo from "@pages/auth/FilmInfo";
 import "@styles/index.css";
 import NavbarGuest from "@components/navbar/guest/NavbarGuest";
+import NavbarAuth from "@components/navbar/auth/NavbarAuth/NavbarAuth";
 
 export default function App() {
   return (
     <Router>
-      <NavbarGuest />
+      <NavbarAuth />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<FilmInfo />} />
--- src/pages/auth/FilmInfo.jsx
+++ src/pages/auth/FilmInfo.jsx
@@ -6,11 +6,13 @@ export default function FilmInfo() {
   const { id } = useParams();
 
   return (
-    <div
-      className="max-w-[1200px] mx-auto px-4 bg-[var(--background-color)] min-h-screen flex items-center justify-center py-10"
-      style={{ color: "#d7e7ee" }}
-    >
-      <CardLg movieId={id} />
-    </div>
+    <>
+      <div
+        className="max-w-[1200px] mx-auto px-4 bg-[var(--background-color)] min-h-screen flex items-center justify-center py-10"
+        style={{ color: "#d7e7ee" }}
+      >
+        <CardLg movieId={id} />
+      </div>
+    </>
   );
 }
