bfd49f2d27208b9271e84fbcd6b3943bb3da4abd
Rudvan <rudvancastro@gmail.com>
2025-05-22 14:31:56 +0200

    Refactor(App): Clean up import statements and improve code formatting for better readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,63 @@
-import {  Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Landing from "@pages/guest/Landing";
 import Home from "@pages/auth/Home";
 import FilmInfo from "@pages/auth/FilmInfo";
-import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from "@auth0/auth0-react";
 import NavbarGuest from "./components/navbar/guest/NavbarGuest.jsx";
 import NavbarAuth from "./components/navbar/auth/NavbarAuth/NavbarAuth.jsx";
 import { useEffect } from "react";
 import ProtectedRoute from "./components/Auth0/ProtectedRoute.jsx";
-import Search from "@pages/auth/Search";
-import Footer from "@components/footer/footer";
+import UserProfile from "@pages/auth/userprofile";
 import "@styles/index.css";
 
-  export default function App() {
-    const { isAuthenticated, isLoading } = useAuth0();
-    const navigate = useNavigate();
+export default function App() {
+  const { isAuthenticated, isLoading } = useAuth0();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-      if (isAuthenticated) {
-        navigate("/home");
-      }
-    }, [isAuthenticated, navigate]);
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/home");
+    }
+  }, [isAuthenticated, navigate]);
 
   if (isLoading) return null;
 
-    return (
-      <>
-        {/* Navbar siempre visible, cambia según autenticación */}
-        {isAuthenticated ? <NavbarAuth /> : <NavbarGuest />}
+  return (
+    <>
+      {/* Navbar siempre visible, cambia según autenticación */}
+      {isAuthenticated ? <NavbarAuth /> : <NavbarGuest />}
 
-          <Routes>
-            {/* Página pública */}
-            <Route path="/" element={<Landing />} />
+      <Routes>
+        {/* Página pública */}
+        <Route path="/" element={<Landing />} />
 
-            {/* Páginas protegidas (logueado) */}
-            <Route
-              path="/home"
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/movie/:id"
-              element={
-                <ProtectedRoute>
-                  <FilmInfo />
-                </ProtectedRoute>
-              }
-            />
+        {/* Páginas protegidas (logueado) */}
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/movie/:id"
+          element={
+            <ProtectedRoute>
+              <FilmInfo />
+            </ProtectedRoute>
+          }
+        />
 
-            <Route 
-              path="/user" 
-              element={
-                <ProtectedRoute>
-                  <UserProfile />
-                </ProtectedRoute>
-                } />
-          </Routes>
-        </>
-    );
-  }
+        <Route
+          path="/user"
+          element={
+            <ProtectedRoute>
+              <UserProfile />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
+}
