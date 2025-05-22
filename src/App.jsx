30aec2f79e00f7a8ba50ed2f373550e59331ad47
LeireNieto <leirenietoripa@gmail.com>
2025-05-22 13:54:53 +0200

    userprofile

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import Landing from "@pages/guest/Landing";
 import Home from "@pages/auth/Home";
 import FilmInfo from "@pages/auth/FilmInfo";
 import "@styles/index.css";
+import UserProfile from "./pages/auth/userprofile";
 
 export default function App() {
   return (
+
     <Router>
+    
+      {/* Rutas de la aplicación */} 
       <Routes>
         {/* Página pública */}
         <Route path="/" element={<Landing />} />
 
         {/* Páginas protegidas (logueado) */}
+        <Route path="/user" element={<UserProfile />} />
         <Route path="/home" element={<Home />} />
         <Route path="/movie/:id" element={<FilmInfo />} />
       </Routes>
