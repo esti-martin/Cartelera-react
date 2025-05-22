8992ccbd1232eca10e17aecb510c2343d7d35e1e
Rudvan <rudvancastro@gmail.com>
2025-05-22 13:36:03 +0200

    style(Footer): Update footer styles to remove background color and enhance layout with consistent black background

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,9 +4,9 @@ import logo from '../../assets/logo.png';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        <img src={logo} alt="Logo" className="footer-logo" />
+    <footer className="footer border-t-2 border-white bg-black">
+      <div className="footer-content bg-black">
+        <img src={logo} alt="Logo" className="footer-logo bg-black" />
         <p className="footer-text">© {new Date().getFullYear()} Todos los derechos reservados</p>
       </div>
     </footer>
