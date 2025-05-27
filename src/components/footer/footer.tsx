3fd70cb59a0a6d7856876194efde9500765a5fb9
Rudvan <rudvancastro@gmail.com>
2025-05-27 13:51:45 +0200

    Update footer component: change logo import path and format footer text for improved readability.

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import './footer.css';
-import logo from '../../assets/logo.png';
+import React from "react";
+import "./footer.css";
+import logo from "../../../public/logo.png";
 
 const Footer = () => {
   return (
     <footer className="footer border-t-2 border-slate-300 dark:border-gray-700 bg-slate-100 dark:bg-zinc-900">
       <div className="footer-content">
         <img src={logo} alt="Logo" className="footer-logo" />
-        <p className="footer-text">© {new Date().getFullYear()} Todos los derechos reservados</p>
+        <p className="footer-text">
+          © {new Date().getFullYear()} Todos los derechos reservados
+        </p>
       </div>
     </footer>
   );
